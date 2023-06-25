68bf94500c464a31ab5478b9971ce553ff89e6a1
root <sai815538@gmail.com>
2023-06-25 10:35:05 +0000

    covid 19 users 1.0

---
+++ app.js
@@ -0,0 +1,277 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "covid19IndiaPortal.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(8000, () => {
+      console.log("Server Running at http://localhost:8000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const objectSnakeToCamel = (newObject) => {
+  return {
+    stateId: newObject.state_id,
+    stateName: newObject.state_name,
+    population: newObject.population,
+  };
+};
+
+const districtSnakeToCamel = (newObject) => {
+  return {
+    districtId: newObject.district_id,
+    districtName: newObject.district_name,
+    stateId: newObject.state_id,
+    cases: newObject.cases,
+    cured: newObject.cured,
+    active: newObject.active,
+    deaths: newObject.deaths,
+  };
+};
+
+const reportSnakeToCamelCase = (newObject) => {
+  return {
+    totalCases: newObject.cases,
+    totalCured: newObject.cured,
+    totalActive: newObject.active,
+    totalDeaths: newObject.deaths,
+  };
+};
+
+function authenticateToken(request, response, next) {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwtToken.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        next();
+      }
+    });
+  }
+}
+
+//
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  console.log(username);
+  const selectUserQuery = `SELECT * FROM user WHERE username = ${username};`;
+  const databaseUser = await db.get(selectUserQuery);
+  if (databaseUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      databaseUser.password
+    );
+    if (isPasswordMatched) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+//get states API1
+app.get("/states/", authenticateToken, async (request, response) => {
+  const getStatesQuery = `
+    SELECT
+    *
+    FROM 
+    state;`;
+
+  const states = await db.all(getStatesQuery);
+
+  const statesResult = states.map((eachObject) => {
+    return objectSnakeToCamel(eachObject);
+  });
+  response.send(statesResult);
+});
+
+//get single state API2
+app.get("/states/:stateId/", authenticateToken, async (request, response) => {
+  const { stateId } = request.params;
+
+  const getStateQuery = `
+    SELECT
+    *
+    FRO
+    state
+    WHERE 
+    state_id = ${stateId};`;
+
+  const states = await db.get(getStateQuery);
+
+  const stateResult = objectSnakeToCamel(states);
+
+  response.send(stateResult);
+});
+
+//create district API3
+app.post("/districts/", authenticateToken, async (request, response) => {
+  const districtDetails = request.body;
+
+  const {
+    districtName,
+    stateId,
+    cases,
+    cured,
+    active,
+    deaths,
+  } = districtDetails;
+
+  const addDistrictQuery = `
+    INSERT INTO district
+    (district_name,state_id,cases,cured,active,deaths)
+    VALUES
+    ('${districtName}',
+        ${stateId},
+        ${cases},
+        ${cured},
+        ${active},
+        ${deaths});`;
+
+  await db.run(addDistrictQuery);
+
+  response.send("District Successfully Added");
+});
+
+//get district API4
+app.get(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+
+    const getDistrictQuery = `
+    SELECT
+    *
+    FROM
+    district
+    WHERE
+    district_id = ${districtId};`;
+
+    const district = await db.get(getDistrictQuery);
+
+    const districtResult = districtSnakeToCamel(district);
+
+    response.send(districtResult);
+  }
+);
+
+//delete district API5
+app.delete(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+
+    const deleteDistrictQuery = `
+    DELETE FROM
+    district
+    WHERE 
+    district_id = ${districtId};`;
+
+    await db.run(deleteDistrictQuery);
+
+    response.send("District Removed");
+  }
+);
+
+//update district API6
+app.put(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+
+    const districtDetails = request.body;
+
+    const {
+      districtName,
+      stateId,
+      cases,
+      cured,
+      active,
+      deaths,
+    } = districtDetails;
+
+    const updateDistrictQuery = `
+    UPDATE 
+    district
+    SET
+    district_name = '${districtName}',
+    state_id = ${stateId},
+    cases = ${cases},
+    cured = ${cured},
+    active = ${active},
+    deaths = ${deaths}
+    WHERE 
+    district_id = ${districtId};`;
+
+    await db.run(updateDistrictQuery);
+
+    response.send("District Details Updated");
+  }
+);
+
+//get statistics of cases API7
+app.get(
+  "/states/:stateId/stats/",
+  authenticateToken,
+  async (request, response) => {
+    const { stateId } = request.params;
+
+    const getStatisticsQuery = `
+    SELECT
+    SUM(cases) AS cases,
+    SUM(cured) AS cured,
+    SUM(active) AS active,
+    SUM(deaths) AS deaths
+    FROM
+    district
+    WHERE 
+    state_id = ${stateId};`;
+
+    const statistics = await db.get(getStatisticsQuery);
+
+    const resultReport = reportSnakeToCamelCase(statistics);
+
+    response.send(resultReport);
+  }
+);
+
+module.exports = app;
